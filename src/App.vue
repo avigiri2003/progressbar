5c351945deca770c14f63d272fed457f02402c4f
Avinash J Giri <avingiri@in.ibm.com>
2022-02-12 14:07:49 +0530

    firstversion

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div>{{percentage}} %</div>
+      <div class="pbar">
+        <div class="progress" :style="{'width': percent + '%'}"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data(){
+    return{
+      percent: 10
+    }
+  },
+  created(){
+    var intval = setInterval(() => {
+      console.log(">>>" + this.percent);
+      if(this.percent < 100)
+        this.percent += 0.1;
+      else
+        clearInterval(intval);
+    }, 10);
+  },
+  computed:{
+    percentage(){
+      return this.percent.toFixed();
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+.container{
+  text-align: center;
+  font-size:2rem;
 }
+.pbar{
+  position: relative;
+  overflow: hidden;
+  width: 300px;
+  height: 20px;
+  border:1px solid blue;
+  border-radius: 15px;
+  box-shadow: 2px 2px 2px grey;
+}
+.progress{
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  border-radius: 15px;
+  background-color: cornflowerblue;
+  /* background-image: linear-gradient(blue, white); */
+  /* background-image: linear-gradient(135deg, white 25%, transparent 25%,
+                      transparent 50%, white 50%,
+                      white 75%, transparent 75%, transparent);
+  background-size: 20px 20px; */
+  animation: animate-stripes 3s linear infinite;
+}
+
+@keyframes animate-stripes {
+  0% { background-position: 0px, 0px;}
+  100% { background-position: 60px, 0px;}
+}
+
 </style>
